829ce4ebc503b2b013d2f7e605aa2b02769e0786
Nathan Douglas <ndouglas@devontechnologies.com>
2014-12-08 22:13:55 -0600

    Adds -[RNCryptor (ReactiveCryptor) rcr_createStreamPairWithBufferSize:inputStream:outputStream:].

--- RNCryptor+ReactiveCryptor.m
+++ RNCryptor+ReactiveCryptor.m
@@ -3,6 +3,7 @@
 //  ReactiveCryptor
 //
 //  Created by Nathan Douglas on 12/8/14.
+//  Inspired by Ari Weinstein's suggested improvements to RNCryptor.
 //  Released into the public domain.
 //  See LICENSE for details.
 //
@@ -11,5 +12,17 @@
 #import "ReactiveCryptor.h"
 
 @implementation RNCryptor (ReactiveCryptor)
+
+- (void)rcr_createStreamPairWithBufferSize:(NSUInteger)bufferSize inputStream:(NSInputStream **)inputStream outputStream:(NSOutputStream **)outputStream {
+    CFReadStreamRef localInputStream;
+    CFWriteStreamRef localOutputStream;
+    CFStreamCreateBoundPair(kCFAllocatorDefault, &localInputStream, &localOutputStream, bufferSize);
+    if (inputStream) {
+        *inputStream = (NSInputStream *)CFBridgingRelease(localInputStream);
+    }
+    if (outputStream) {
+        *outputStream = (NSOutputStream *)CFBridgingRelease(localOutputStream);
+    }
+}
 
 @end
