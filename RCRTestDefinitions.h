011965b47c5cbb2a6fc0cb559aec38be56fb8ad7
Nathan Douglas <ndouglas@devontechnologies.com>
2014-12-09 12:21:13 -0600

    Adds XCTest (ReactiveCryptor).

---
+++ RCRTestDefinitions.h
@@ -0,0 +1,50 @@
+//
+//  RCRTestDefinitions.h
+//  ReactiveCryptor
+//
+//  Created by Nathan Douglas on 12/9/14.
+//  Released into the public domain.
+//  See LICENSE for details.
+//
+
+#import <XCTest/XCTest.h>
+#import "ReactiveCryptor.h"
+
+@interface XCTestCase (ReactiveCryptor)
+
+/**
+ Subscribes to the signal and succeeds if the signal then sends a completion before the specified timeout.
+ 
+ @param signal A signal to test.
+ @param timeout A timeout within which the signal must complete.
+ @param description A description of this test.
+ @discussion Any non-completion values are treated as failures.
+ */
+
+- (void)rcr_expectCompletionFromSignal:(RACSignal *)signal timeout:(NSTimeInterval)timeout description:(NSString *)description;
+
+/**
+ Subscribes to the signal and succeeds if the signal then sends a new value before the specified timeout.
+ 
+ @param nextHandler A block that can test the next value further.
+ @param signal A signal to test.
+ @param timeout A timeout within which the signal must complete.
+ @param description A description of this test.
+ @discussion Any non-next values are treated as failures.
+ */
+
+- (void)rcr_expectNext:(void (^)(id next))nextHandler signal:(RACSignal *)signal timeout:(NSTimeInterval)timeout description:(NSString *)description;
+
+/**
+ Subscribes to the signal and succeeds if the signal then sends an error before the specified timeout.
+ 
+ @param errorHandler A block that can test the error further.
+ @param signal A signal to test.
+ @param timeout A timeout within which the signal must send an error.
+ @param description A description of this test.
+ @discussion Any non-error values are treated as failures.
+ */
+
+- (void)rcr_expectError:(void (^)(NSError *error))errorHandler signal:(RACSignal *)signal timeout:(NSTimeInterval)timeout description:(NSString *)description;
+
+@end
---
+++ RCRTestDefinitions.m
@@ -0,0 +1,102 @@
+//
+//  RCRTestDefinitions.m
+//  ReactiveCryptor
+//
+//  Created by Nathan Douglas on 12/9/14.
+//  Released into the public domain.
+//  See LICENSE for details.
+//
+
+#import "RCRTestDefinitions.h"
+
+@implementation XCTestCase (ReactiveCryptor)
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeNext:(void (^)(id next))nextHandler error:(void (^)(NSError *error))errorHandler completion:(void (^)(void))completionHandler timeout:(NSTimeInterval)timeout {
+    __block BOOL nextEncountered = NO;
+    nextHandler = nextHandler ?: ^(id next) {
+        XCTFail(@"Expectation '%@' for signal '%@' received unexpected next: %@", expectation.description, signal, next);
+    };
+    errorHandler = errorHandler ?: ^(NSError *error) {
+        XCTFail(@"Expectation '%@' for signal '%@' received unexpected error: %@", expectation.description, signal, error);
+    };
+    completionHandler = completionHandler ?: ^{
+        XCTFail(@"Expectation '%@' for signal '%@' received unexpected completion.", expectation.description, signal);
+    };
+    RACDisposable *disposable = [[signal
+    take:1]
+    subscribeNext:^(id next) {
+        if (!nextEncountered) {
+            nextEncountered = YES;
+            nextHandler(next);
+        } else {
+            NSLog(@"Ignoring post-test next '%@' for expectation '%@' for signal '%@'.", next, expectation.description, signal);
+        }
+    } error:^(NSError *error) {
+        if (!nextEncountered) {
+            errorHandler(error);
+        } else {
+            NSLog(@"Ignoring post-test error '%@' for expectation '%@' for signal '%@'.", error.localizedDescription, expectation.description, signal);
+        }
+    } completed:^{
+        if (!nextEncountered) {
+            completionHandler();
+        } else {
+            NSLog(@"Ignoring post-test completion for expectation '%@' for signal '%@'.", expectation.description, signal);
+        }
+    }];
+    [self waitForExpectationsWithTimeout:timeout handler:^(NSError *error) {
+        if (error) {
+            XCTFail(@"Expectation '%@' for signal '%@' failed with error: %@", expectation.description, signal, error);
+        }
+        [disposable dispose];
+    }];
+}
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeNext:(void (^)(id))nextHandler error:(void (^)(NSError *))errorHandler timeout:(NSTimeInterval)timeout {
+    [self rcr_expectation:expectation signal:signal subscribeNext:nextHandler error:errorHandler completion:nil timeout:timeout];
+}
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeNext:(void (^)(id))nextHandler completion:(void (^)(void))completionHandler timeout:(NSTimeInterval)timeout {
+    [self rcr_expectation:expectation signal:signal subscribeNext:nextHandler error:nil completion:completionHandler timeout:timeout];
+}
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeNext:(void (^)(id))nextHandler timeout:(NSTimeInterval)timeout {
+    [self rcr_expectation:expectation signal:signal subscribeNext:nextHandler error:nil completion:nil timeout:timeout];
+}
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeError:(void (^)(NSError *))errorHandler completion:(void (^)(void))completionHandler timeout:(NSTimeInterval)timeout {
+    [self rcr_expectation:expectation signal:signal subscribeNext:nil error:errorHandler completion:completionHandler timeout:timeout];
+}
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeError:(void (^)(NSError *))errorHandler timeout:(NSTimeInterval)timeout {
+    [self rcr_expectation:expectation signal:signal subscribeNext:nil error:errorHandler completion:nil timeout:timeout];
+}
+
+- (void)rcr_expectation:(XCTestExpectation *)expectation signal:(RACSignal *)signal subscribeCompletion:(void (^)(void))completionHandler timeout:(NSTimeInterval)timeout {
+    [self rcr_expectation:expectation signal:signal subscribeNext:nil error:nil completion:completionHandler timeout:timeout];
+}
+
+- (void)rcr_expectCompletionFromSignal:(RACSignal *)signal timeout:(NSTimeInterval)timeout description:(NSString *)description {
+    XCTestExpectation *expectation = [self expectationWithDescription:description];
+    [self rcr_expectation:expectation signal:signal subscribeCompletion:^{
+        [expectation fulfill];
+    } timeout:timeout];
+}
+
+- (void)rcr_expectNext:(void (^)(id next))nextHandler signal:(RACSignal *)signal timeout:(NSTimeInterval)timeout description:(NSString *)description {
+    XCTestExpectation *expectation = [self expectationWithDescription:description];
+    [self rcr_expectation:expectation signal:signal subscribeNext:^(id next) {
+        nextHandler(next);
+        [expectation fulfill];
+    } timeout:timeout];
+}
+
+- (void)rcr_expectError:(void (^)(NSError *error))errorHandler signal:(RACSignal *)signal timeout:(NSTimeInterval)timeout description:(NSString *)description {
+    XCTestExpectation *expectation = [self expectationWithDescription:description];
+    [self rcr_expectation:expectation signal:signal subscribeError:^(NSError *error) {
+        errorHandler(error);
+        [expectation fulfill];
+    } timeout:timeout];
+}
+
+@end
